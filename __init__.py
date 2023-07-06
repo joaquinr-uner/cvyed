d27ab201fa58a9de4e93c7ef75d233b8c7018ff9
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-07-06 12:34:23 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+from scipy.spatial import Delaunay
+
 from .surf2stl import tri_write
 from .intervalo import intervalo
 from .plot3d_parametric_line_CV import plot3d_parametric_line_CV
