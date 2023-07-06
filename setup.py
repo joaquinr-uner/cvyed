b225633210681994e57ebed249ccb39080a89101
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-07-06 11:56:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
-from setuptools import setup
+#from setuptools import setup
 
-setup(
-    name="CV",
-    packages=[
-        "CV",
-        "CV.dominio",
-        "CV.esfera",
-        "CV.fig_ejes3d_cy",
-        "CV.fig_ejes_cv.py",
-        "CV.gradiente",
-        "CV.intervalo",
-        "CV.plot3d_parametric_line_CV",
-        "CV.plot_implicit3d_cv",
-        "CV.punte3d_cv",
-        "CV.punto_cv",
-        "CV.sup_nivel_cv",
-        "CV.surf2stl",
-        "CV.tubo_3d",
-        "CV.vector3d_cv",
-        "CV.vector_cv",
-    ],
-    install_requires=[
-        "numpy",
-        "sympy",
-        "matplotlib",
-        "scipy",
-        "git+https://github.com/asahidari/surf2stl-python",
-    ],
-    author="CVyED - FIUNER",
-)
+#setup(
+#    name="CV",
+#    packages=[
+#        "CV",
+#        "CV.dominio",
+#        "CV.esfera",
+#        "CV.fig_ejes3d_cy",
+#        "CV.fig_ejes_cv.py",
+#        "CV.gradiente",
+#        "CV.intervalo",
+#        "CV.plot3d_parametric_line_CV",
+#        "CV.plot_implicit3d_cv",
+#        "CV.punte3d_cv",
+#        "CV.punto_cv",
+#        "CV.sup_nivel_cv",
+#        "CV.surf2stl",
+#        "CV.tubo_3d",
+#        "CV.vector3d_cv",
+#        "CV.vector_cv",
+#    ],
+#    install_requires=[
+#        "numpy",
+#        "sympy",
+#        "matplotlib",
+#        "scipy",
+#        "git+https://github.com/asahidari/surf2stl-python",
+#    ],
+#    author="CVyED - FIUNER",
+#)
