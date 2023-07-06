02d9b0b4f060614b981dc5980ced4f304ace178e
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-07-06 12:50:56 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
 from scipy.spatial import Delaunay
 
-from .dominio.py import dominio
-from .esfera.py import esfera
-from .fig_ejes3d_cv.py import fig_ejes3d_cv
-from .fig_ejes_cv.py import fig_ejes_cv
-from .gradiente.py import gradiente
-from .intervalo.py import intervalo
 from .dominio import dominio
+from .esfera import esfera
+from .fig_ejes3d_cv import fig_ejes3d_cv
+from .fig_ejes_cv import fig_ejes_cv
+from .gradiente import gradiente
+from .intervalo import intervalo
+from .plot3d_parametric_line_CV import plot3d_parametric_line_CV
