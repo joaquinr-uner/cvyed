0c0f9da0a4697162fe7f2b60dea44adfbd467ac7
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-08-22 15:49:50 -0300

    Create sum_par.py

---
+++ sum_par.py
@@ -0,0 +1,13 @@
+def sum_par(a_n,inicio,final):
+    # definimos la sumatora que va desde el inicio al valor final.
+
+    n = sp.Symbol('n')
+    lista = []
+    #Define una variable donde se acumula la suma
+    suma = 0
+
+    #definimos una iteracion para sumar los terminos de la serie
+    for i in range(inicio,final+1):
+        suma += a_n.subs(n,i)
+        lista.append(suma)
+    return np.array(lista)
