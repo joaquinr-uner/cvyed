d4b7547871adad2f1cc27770c857044b9c0353d3
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-08-22 15:01:58 -0300

    Update grafica_sucesion.py

--- grafica_sucesion.py
+++ grafica_sucesion.py
@@ -22,12 +22,12 @@ def grafica_sucesion(nn,A_n,ejes = None, label = ''):
         axes = fig.gca()
         #Graficamos (mediante puntos) sucesión {1/n} en el intervalo que creamos: (k,k+n) con pasos de 1
         #axes.scatter(n, a_n, label="1/n",s = 10)
-        axes.plot(ni, a_n,label=label,marker='o',ls='--')# ls = estilo de linea
+        axes.plot(nn, a_n,label=label,marker='o',ls='--')# ls = estilo de linea
         #Definimos las etiquetasde los ejes
         plt.xlabel('n')
         plt.ylabel('a_n')
         #Define el rango de visualizacion de n
-        axes.set_xlim(n[0],ni[len(n)-1])
+        axes.set_xlim(n[0],nn[len(n)-1])
         #Agrega una grilla
         axes.grid()
         #Agrega una leyenda
