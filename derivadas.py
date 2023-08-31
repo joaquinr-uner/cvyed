adc49a99fc9464a591b31273e449d19be5ae3090
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-08-31 20:33:55 -0300

    Update derivadas.py

--- derivadas.py
+++ derivadas.py
@@ -1,6 +1,8 @@
 def derivadas(func,orden):
+    for sym in f.free_symbols:
+        t = sym
     derivadas = []
     #derivada[0]=func
     for i in range(orden):
-        derivadas.append(func.diff(x,i))
+        derivadas.append(func.diff(t,i))
     return derivadas
