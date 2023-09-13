01e3590d1faf6008b427078ed0442ead6e064b03
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-09-13 19:22:08 -0300

    Update vector_cv.py

--- vector_cv.py
+++ vector_cv.py
@@ -20,17 +20,17 @@ def vector_cv (x1,x2,y1,y2,ejes = None,color='b',x_min=-5,x_max=5,y_min=-5,y_max
         ax.quiver(x1,x2,(y1-x1),(y2-x2), color = color, angles='xy', scale_units='xy', scale=1)
         return None
     else:
-        ax.set_aspect('equal')
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
+        ejes.set_aspect('equal')
+        ejes.set_xlabel('x')
+        ejes.set_ylabel('y')
         for spine in ["left", "bottom"]:
-            ax.spines[spine].set_position("zero")
+            ejes.spines[spine].set_position("zero")
 
         for spine in ["right", "top"]:
-            ax.spines[spine].set_color("none")
-        ax.grid()
-        ax.set_xlim(x_min,x_max)
-        ax.set_ylim(y_min,y_max)
+            ejes.spines[spine].set_color("none")
+        ejes.grid()
+        ejes.set_xlim(x_min,x_max)
+        ejes.set_ylim(y_min,y_max)
         out = ejes.quiver(x1,x2,(y1-x1),(y2-x2), color = color, angles='xy', scale_units='xy', scale=1)
         return out
         
