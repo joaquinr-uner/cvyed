742904eec2c944411cde7dadce3c9eaa2f9b91fb
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-09-16 10:43:46 -0300

    Update grilla_2d.py

--- grilla_2d.py
+++ grilla_2d.py
@@ -6,8 +6,8 @@ def grilla_2d (x_min=0,x_max=0,y_min=0,y_max=0,cantidad=5):
     if x_min == 0 and x_max == 0 and y_min == 0 and y_max == 0:
         print("Falta definir dominio")
     else:
-        tmp = np.mgrid[x_min:x_max+pasox:pasox,y_min:y_max+pasoy:pasoy]
-        D['x'] =tmp[0]
-        D['y'] =tmp[1]
+        tmp = np.mgrid[x_min:x_max:pasox,y_min:y_max:pasoy]
+        D['x'] =tmp[0].T
+        D['y'] =tmp[1].T
                 
     return D['x'],D['y']
