92a8e2ea5f92871588eead03d95b3880c8048570
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-09-19 15:11:08 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,6 +20,8 @@ from .vector_cv import vector_cv
 from .surf2stl import tri_write
 from .plot_implicit_3d_cv import plot_implicit_3d_cv
 from .export3d_parametric_surface import export3d_parametric_surface
+from .grilla_2d import grilla_2d
+from .grilla_3d import grilla_3d
 
 from .indice import indice
 from .euler import euler
