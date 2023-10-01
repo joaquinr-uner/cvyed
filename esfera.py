b0d51defbc08056263930cad0efd834889137c92
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-10-01 11:09:44 -0300

    Update esfera.py

--- esfera.py
+++ esfera.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+from .surf2stl import tri_write
+from scipy.spatial import Delaunay
 def esfera(x,y,z,radio=1, ejes = None, archivo = None):
     """Genera una esfera de radio r con centro en (x,y,z."""
     """el nombre del archivo debe contener la extensión .stl y escribirse entre comillas simples 'archivo.stl'"""
