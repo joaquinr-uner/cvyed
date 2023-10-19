5a4c12e876a6508a33821fea9454a76bd8fede21
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-10-19 09:58:37 -0300

    Create serieFourier.m

---
+++ serieFourier.m
@@ -0,0 +1,10 @@
+def serieFourier(x,ft,L,n):
+    a0 = 1/L*sp.integrate(ft,(x,-L,L))
+    am = 1/L*sp.integrate(ft*sp.cos(m*sp.pi*x/L),(x,-L,L))
+    bm = 1/L*sp.integrate(ft*sp.sin(m*sp.pi*x/L),(x,-L,L))
+
+    serie = (a0/2)
+    for i in range(1, n+1):
+        i
+        serie = serie + (am*sp.cos(m*sp.pi*x/L)+(bm*sp.sin(m*sp.pi*x/L))).subs(m, i)
+    return serie
