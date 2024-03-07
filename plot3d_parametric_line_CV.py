5181d259c80b71931b7c077cf9bc2ac0ce06a171
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2024-03-07 19:48:17 -0300

    Update plot3d_parametric_line_CV.py

--- plot3d_parametric_line_CV.py
+++ plot3d_parametric_line_CV.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def plot3d_parametric_line_CV(X=0,Y=0,Z=0,p=np.linspace(0, 2,100),ejes = None):
-    x = np.ones_like(p)*X
-    y = np.ones_like(p)*Y
-    z = np.ones_like(p)*Z
-    if(ejes == None):
-        fig = plt.figure()
-        ax3d = fig.addsubplots(projection='3d')
-        ax3d.plot(x, y, z)
+    if X is not
+    X1_ = sp.lambdify(t, X, modules='numpy')
+    X2_ = sp.lambdify(t, Y, modules='numpy')
+    X3_ = sp.lambdify(t, Z, modules='numpy')
     
-        return None
-    else:
-        out = ejes.plot(x, y, z)
-        return out
+    t_num = np.linspace(float(t1), float(t2), endpoint=True, num=100)
+    
+    X1_num = np.array([X1_(t_num[i]) for i in range(100)])
+    X2_num = np.array([X2_(t_num[i]) for i in range(100)])
+    X3_num = np.array([X3_(t_num[i]) for i in range(100)])
+    
+    
+    #x = np.ones_like(p)*X
+    #y = np.ones_like(p)*Y
+    #z = np.ones_like(p)*Z
     
     
     
@@ -28,4 +31,3 @@ def plot3d_parametric_line_CV(X=0,Y=0,Z=0,p=np.linspace(0, 2,100),ejes = None):
     else:
         out = ejes.plot(X1_num, X2_num, X3_num)
         return out
-        
