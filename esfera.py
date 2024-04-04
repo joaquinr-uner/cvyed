fc61de09250c0cda01f1f84cdedafaebb4b72d6c
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2024-04-04 18:20:33 -0300

    Update esfera.py

--- esfera.py
+++ esfera.py
@@ -27,7 +27,7 @@ def esfera(x,y,z,radio=1, ejes = None, archivo = None):
     
     if(ejes == None):
         fig = plt.figure()
-        ax = fig.gca(projection='3d')
+        ax = fig.add_subplot(111,projection='3d')
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
