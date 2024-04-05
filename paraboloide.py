47868e94c6cbff753c8418e25e378f2dd4ac4bdf
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2024-04-05 16:45:02 -0300

    Update paraboloide.py

--- paraboloide.py
+++ paraboloide.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
 from .surf2stl import tri_write
 from scipy.spatial import Delaunay
-def paraboloide(a=1,b=1, xc = 0, yc = 0, zc = 0, ejes = None, archivo = None):
-    """Genera un paraboloide elíptico de ecuación z = (x/a)**2+(y/a)**2 con centro en (xc,yc,zc)."""
+def paraboloide(a=1,b=1, h=1, xc = 0, yc = 0, zc = 0, ejes = None, archivo = None):
+    """Genera un paraboloide elíptico de ecuación z = (x/a)**2+(y/a)**2 con centro en (xc,yc,zc) y altura h."""
     """el nombre del archivo debe contener la extensión .stl y escribirse entre comillas simples 'archivo.stl'"""
     
-    r = np.linspace(0, 2.0 * np.pi, endpoint=True, num=100)
+    r = np.linspace(0, sp.sqrt(t), endpoint=True, num=100)
     v = np.linspace(0, 2.0 * np.pi, endpoint=True, num=100)
     r, v = np.meshgrid(r, v)
     r, v = r.flatten(), v.flatten()
