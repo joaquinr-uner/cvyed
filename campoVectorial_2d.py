e5c1d69b13472195aed5c5819989efa8866e4c88
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2024-05-30 11:52:14 -0300

    Update campoVectorial_2d.py

--- campoVectorial_2d.py
+++ campoVectorial_2d.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .grilla_2d import grilla_2d
 def campoVectorial_2d (F1,F2,X1,X2,ejes = None,color='b', angles='xy', scale_units='xy', scale=1, npuntos = 10):
+    # F1, F2: Componentes del campo vectorial 2d
+    # X1, X2: variables y rango de variación. Debe pasarse como una tupla con estructura (x, x_min, x_max)
     #posibles colores: 'b' blue 'g' green 'r' red 'c' cyan 'm' magenta 'y' yellow 'k' black 'w' white
-    #Las expresiones de P y Q deben ser escritas con funciones de numpy. Por ejemplo: np.cos() y ademas contener las variables que se definan como dominio.
     
     x1,x1_min,x1_max = X1
     x2,x2_min,x2_max = X2
