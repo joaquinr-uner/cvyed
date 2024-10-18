bbcd8d2d060dc3834406de04884a700627e912b6
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2024-10-18 14:32:48 -0300

    Update punto3d_cv.py

--- punto3d_cv.py
+++ punto3d_cv.py
@@ -3,7 +3,7 @@ def punto3d_cv (x1,x2,x3,ejes = None,color='r',x_min=-5,x_max=5,y_min=-5,y_max=5
     #posibles colores: 'b' blue 'g' green 'r' red 'c' cyan 'm' magenta 'y' yellow 'k' black 'w' white
     if(ejes == None):
         fig = plt.figure()
-        ax = fig.gca(projection='3d')
+        ax = fig.add_subplot(111, projection="3d")
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
